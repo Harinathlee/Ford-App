1c2e988d067c6a3c3073d74b59a539f046391cc4
Raviraj Parab <ravirajparab44@gmail.com>
2024-09-18 15:04:06 +0530

    reads base url from the env

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,6 @@ import { Provider } from "react-redux";
 import store from "./store/store";
 import "./index.css";
 
-// reading the configs
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
-const appConfigUrl = import.meta.env.VITE_APP_CONFIG_URL;
-console.log(`
-  apiBaseURL : ${apiBaseUrl} 
-  appConfigURL : ${appConfigUrl}
-  `)
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
